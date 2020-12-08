2e76fa145a0e18e78db7626e544a67a34bb2598b
Mateus Dreher <dev.mateusdreher@gmail.com>
2020-12-08 20:52:51 -0300

    Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# FrontEnd do teste QlickDev:
+
+## Instructions
+
+Aplicação em produção: [app.testQuickDev](http://app-filmow.herokuapp.com)
+
+Rodar local: apenas abra o arquivo ```pages/index.html``` no navegador.
+
+## Explanation
+
+A página consite em listar os filmes mais populares e ao clicar em um deles, será redirecionado para uma página contendo mais detalhes.
+
+### Features
+
+* Filtrar por nome ou genero de filme.
--- js/request.js
+++ js/request.js
@@ -1,12 +1,12 @@
-const BASE_URI = 'https://api-filmow.herokuapp.com/api/movies';
+const BASE_URI = `${location.protocol}//api-filmow.herokuapp.com/api/movies`;
 function request(endpoint) {
     showLoader();
     let url = `${BASE_URI}/${endpoint}`;
-
+    console.log(url);
     var xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.open('GET', url, false);
     xmlHttpRequest.send(null);
-   
+    
     if (xmlHttpRequest.status === 200) {
         hideLoader();
         return JSON.parse(xmlHttpRequest.responseText);
